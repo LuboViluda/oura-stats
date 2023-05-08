99bb4c0a9c25eb947119d550089122dd1120711c
Lubo Viluda <lubo.viluda@gmail.com>
2023-05-08 20:52:43 +0200

    add baseline calculation and diff calculation against baseline

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 - script to get daily sleep stats from API and compute weekly average of values "light_sleep_duration", "rem_sleep_duration, "average_hrv"
 and "average_heart_rate"
+- compute averages for selected base line weeks
+- compute diff of each week against baseline weeks
 - save computed values as csv. and json values for next processing/visualization  
 - handy to examinate impact of self experiment on sleet  
 
--- main.py
+++ main.py
@@ -61,10 +61,14 @@ def build_stats():
         if d["average_hrv"] is not None:
             week_dict[week]["average_heart_rate"].append(d["average_heart_rate"])
 
-    weekly_averages = []
+    all_weekly_averages = []
+    baseline_weekly_averages = []
     count = 1
+
+    # TODO store only in fload
+    # to str only for print
     for week, data in week_dict.items():
-        weekly_averages.append({
+        weekly_averages = {
             "week": count,
             "week-start": week,
             "deep_sleep": "{:.2f}".format(sum(data["deep_sleep"]) / len(data["deep_sleep"])),
@@ -72,24 +76,67 @@ def build_stats():
             "rem_sleep": "{:.2f}".format(sum(data["rem_sleep"]) / len(data["rem_sleep"])),
             "average_hrv": "{:.2f}".format(sum(data["average_hrv"]) / len(data["average_hrv"])),
             "average_heart_rate": "{:.2f}".format(sum(data["average_heart_rate"]) / len(data["average_heart_rate"]))
-        })
+        }
+        all_weekly_averages.append(weekly_averages)
+        if "2023-01-16" <= week <= "2023-01-23":
+            baseline_weekly_averages.append(weekly_averages)
+
         count = count + 1
 
+    # average on baseline
+    baseline_averages = {
+            "week": 0,
+            "week-start": 'avg',
+            "deep_sleep": "{:.2f}".format(calculate_field_avg(baseline_weekly_averages, "deep_sleep")),
+            "light_sleep": "{:.2f}".format(calculate_field_avg(baseline_weekly_averages, "light_sleep")),
+            "rem_sleep": "{:.2f}".format(calculate_field_avg(baseline_weekly_averages, "rem_sleep")),
+            "average_hrv": "{:.2f}".format(calculate_field_avg(baseline_weekly_averages, "average_hrv")),
+            "average_heart_rate": "{:.2f}".format(calculate_field_avg(baseline_weekly_averages, "average_heart_rate"))
+        }
+
+    differences = []
+    for weekly_average in all_weekly_averages:
+        difference = {
+            "week": weekly_average["week"],
+            "week-start": weekly_average["week-start"],
+            "deep_sleep_diff": "{:.2f}".format(float(weekly_average["deep_sleep"]) - float(baseline_averages["deep_sleep"])),
+            "light_sleep_diff": "{:.2f}".format(float(weekly_average["light_sleep"]) - float(baseline_averages["light_sleep"])),
+            "rem_sleep_diff": "{:.2f}".format(float(weekly_average["rem_sleep"]) - float(baseline_averages["rem_sleep"])),
+            "average_hrv_diff": "{:.2f}".format(float(weekly_average["average_hrv"]) - float(baseline_averages["average_hrv"])),
+            "average_heart_rate_diff": "{:.2f}".format(float(weekly_average["average_heart_rate"]) - float(baseline_averages["average_heart_rate"]))
+        }
+        differences.append(difference)
+
     # save the weekly averages as a JSON file
-    weekly_averages_json = json.dumps(weekly_averages)
-    with open("weekly_averages.json", "w") as f:
+    weekly_averages_json = json.dumps(all_weekly_averages)
+    with open("all_weekly_averages.json", "w") as f:
         f.write(weekly_averages_json)
 
     # save the weekly averages as a CSV file
-    with open("weekly_averages.csv", "w", newline="") as f:
+    with open("all_weekly_averages.csv", "w", newline="") as f:
         writer = csv.DictWriter(f, fieldnames=["week", "week-start", "deep_sleep", "light_sleep", "rem_sleep",
                                                "average_hrv",
                                                "average_heart_rate"])
         writer.writeheader()
-        for d in weekly_averages:
+        for d in all_weekly_averages:
             writer.writerow(d)
 
-    df = pd.read_csv('weekly_averages.csv')
+
+    # save the differences as a JSON file
+    differences_json = json.dumps(differences)
+    with open("weekly_differences.json", "w") as f:
+        f.write(differences_json)
+
+    # save the weekly averages as a CSV file
+    with open("weekly_differences.csv", "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["week", "week-start", "deep_sleep_diff", "light_sleep_diff", "rem_sleep_diff",
+                                               "average_hrv_diff",
+                                               "average_heart_rate_diff"])
+        writer.writeheader()
+        for d in differences:
+            writer.writerow(d)
+
+    df = pd.read_csv('all_weekly_averages.csv')
 
     # Select column to plot
     x = df['deep_sleep']
@@ -124,6 +171,18 @@ def seconds_to_minutes(duration_seconds: int) -> int:
     minutes = duration_seconds // 60
     return minutes
 
+def calculate_field_avg(data_list, field):
+    total_sum = 0.0
+    count = 0
+    for item in data_list:
+        if field in item:
+            total_sum += float(item[field])
+            count = count + 1
+
+    if count == 0:
+        return 0
+
+    return total_sum / count
 
 if __name__ == '__main__':
     build_stats()
